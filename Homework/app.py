1c73935d63fccdcf62a2997d1c842121da32b0ba
Ramya LS <ramya.sivaprakasam@gmail.com>
2021-08-13 11:54:39 -0400

    HW5

--- Homework/app.py
+++ Homework/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify
 import sqlalchemy
 from sqlalchemy import create_engine, func
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 import datetime as dt
 
-#################################################
+
+
 # Database Setup
-#################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # Reflect database into a new model
@@ -18,9 +18,82 @@ Base.prepare(engine, reflect=True)
 
 # Save reference to the tables
 Measurement = Base.classes.measurement
-
 Station = Base.classes.station
 
-# Create session
-session = Session(engine)
+# Flask Setup
 
+app = Flask(__name__)
+
+# Flask Routes
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+
+    )
+
+@app.route("/api/v1.0/stations")    
+def stations():
+    session = Session(engine)
+    sel = [Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    stations = []
+    for station,name,lat,lon,el in queryresult:
+        station_dict = {}
+        station_dict["Station"] = station
+        station_dict["Name"] = name
+        station_dict["Lat"] = lat
+        station_dict["Lon"] = lon
+        station_dict["Elevation"] = el
+        stations.append(station_dict)
+
+    return jsonify(stations)
+     
+    
+    
+@app.route("/api/v1.0/precipitation")    
+def precipitation():
+    session = Session(engine)
+    """Return the precipitation data for the last year"""
+    
+   # Calculate the date 1 year ago from last date in database
+    date_yr_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+    
+ # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= date_yr_ago).all()
+   # Dict with date as the key and prcp as the value
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route('/api/v1.0/tobs')
+def tobs():
+    session = Session(engine)
+    
+    # Calculate the date 1 year ago from last date in database
+    date_yr_ago = dt.date(2017,8,23) - dt.timedelta(days=365)
+    tob_data = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date >= date_yr_ago).all()
+    session.close()
+
+    
+    tobs_list = []
+    for result in tob_data:
+        temp = {}
+        temp["date"] = result[1]
+        temp["temprature"] = result[0]
+        tobs_list.append(temp)
+    return jsonify(tobs_list)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
